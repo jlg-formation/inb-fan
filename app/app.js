129193aaf96e1063778c7bfb4a3c495d7afdce5b
llesecq <llesecq@inbox.fr>
2017-04-03 14:47:12 +0200

    add: directives

---
+++ app/app.js
@@ -0,0 +1,24 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('main', []);
+
+	app.directive('inbHeader', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/inb-header.html'
+		};
+	});
+    app.directive('inbBody', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/inb-body.html'
+		};
+	});
+    app.directive('inbFooter', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/inb-footer.html'
+		};
+	});
+})();

271df6af41df84000da423fceab0345bfe47a068
llesecq <llesecq@inbox.fr>
2017-04-04 14:59:41 +0200

    add: note

--- app/inb-stars/inb-stars.js
+++ app/inb-stars/inb-stars.js
@@ -8,12 +8,13 @@
 			restrict: 'E',
 			template: function(element, attrs) {
 				console.log('inbStars template', arguments);
+				var note = ('note' in attrs) ? attrs.note : 3;
 				var result = '';
-				for (let i = 0; i < 3; i++) {
+				for (let i = 0; i < note; i++) {
 					result += '<img src="./inb-stars/img/yellow_star.png"/>';
 				}
 
-				for (let i = 3; i < 5; i++) {
+				for (let i = note; i < 5; i++) {
 					result += '<img src="./inb-stars/img/white_star.png"/>';
 				}
 				return result;

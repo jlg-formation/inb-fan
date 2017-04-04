c1b1e384b362e863857b07cfaf0da1d62a07547e
llesecq <llesecq@inbox.fr>
2017-04-04 15:34:12 +0200

    add: update click

--- app/inb-stars/inb-stars.js
+++ app/inb-stars/inb-stars.js
@@ -9,22 +9,28 @@
 			scope: {
 				n: '=note'
 			},
-			link: function (scope, element) {
+			controller: function ($scope, $element, $compile) {
+				'ngInject';
 				console.log('inbStars template', arguments);
-				scope.$watch('n', function () {
-					var note = (scope.n !== undefined) ? Number(scope.n) : 3;
+				$scope.update = function (newNote) {
+					console.log('update', arguments);
+					$scope.n = newNote;
+				};
+				$scope.$watch('n', function () {
+					var note = ($scope.n !== undefined) ? Number($scope.n) : 3;
 					note = (isNaN(note)) ? 3 : note;
 					note = (note > 5) ? 5 : note;
 					note = (note < 0) ? 0 : note;
 					var html = '';
 					for (let i = 0; i < note; i++) {
-						html += '<img src="./inb-stars/img/yellow_star.png"/>';
+						html += '<img ng-click="update(' + (i + 1) + ')" src="./inb-stars/img/yellow_star.png"/>';
 					}
 
 					for (let i = note; i < 5; i++) {
-						html += '<img src="./inb-stars/img/white_star.png"/>';
+						html += '<img ng-click="update(' + (i + 1) + ')" src="./inb-stars/img/white_star.png"/>';
 					}
-					element.html(html);
+					$element.html(html);
+					$compile($element.contents())($scope);
 				});
 
 			}

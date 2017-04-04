b3c0771fdf3f6ca95dc93f279927572b2749a6fa
llesecq <llesecq@inbox.fr>
2017-04-04 15:22:20 +0200

    add: watch

--- app/inb-stars/inb-stars.js
+++ app/inb-stars/inb-stars.js
@@ -6,21 +6,27 @@
 	app.directive('inbStars', function () {
 		return {
 			restrict: 'E',
-			template: function (element, attrs) {
+			scope: {
+				n: '=note'
+			},
+			link: function (scope, element) {
 				console.log('inbStars template', arguments);
-				var note = ('note' in attrs) ? Number(attrs.note) : 3;
-				note = (isNaN(note)) ? 3 : note;
-				note = (note > 5) ? 5 : note;
-				note = (note < 0) ? 0 : note;
-				var result = '';
-				for (let i = 0; i < note; i++) {
-					result += '<img src="./inb-stars/img/yellow_star.png"/>';
-				}
+				scope.$watch('n', function () {
+					var note = (scope.n !== undefined) ? Number(scope.n) : 3;
+					note = (isNaN(note)) ? 3 : note;
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
+					var html = '';
+					for (let i = 0; i < note; i++) {
+						html += '<img src="./inb-stars/img/yellow_star.png"/>';
+					}
 
-				for (let i = note; i < 5; i++) {
-					result += '<img src="./inb-stars/img/white_star.png"/>';
-				}
-				return result;
+					for (let i = note; i < 5; i++) {
+						html += '<img src="./inb-stars/img/white_star.png"/>';
+					}
+					element.html(html);
+				});
+
 			}
 		};
 	});

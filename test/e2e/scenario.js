a4841ccaa5d933366d78803101c0d28afe13fd0e
llesecq <llesecq@inbox.fr>
2017-04-05 15:50:12 +0200

    add: protractor ajouts nouveaux test

--- test/e2e/scenario.js
+++ test/e2e/scenario.js
@@ -1,16 +1,37 @@
 describe('Selenium Test Case', function() {
-  it('should execute test case without errors', function() {
-    var text, value, bool, source, url, title;
-    var TestVars = {};
-    browser.get("http://localhost:8000/app/");
-    var img1 = element(by.xpath("//inb-body/ui-view/inb-stars[1]/img[5]")).click();
-    expect(img1.getAttribute('src')).toContain('yellow');
-    element(by.xpath("//inb-body/ui-view/inb-stars[2]/img[5]")).click();
-    element(by.xpath("//inb-body/ui-view/inb-stars[4]/img[5]")).click();
-    element(by.xpath("//inb-body/ui-view/inb-stars[1]/img[5]")).click();
-    element(by.xpath("//inb-body/ui-view/inb-stars[5]/img[5]")).click();
+	beforeEach(function() {
+		browser.get("http://localhost:8000/app/");
+	});
+	it('should execute test case without errors', function() {
+		var text, value, bool, source, url, title;
+		var TestVars = {};
 
-    element(by.linkText("Produits")).click();
-    element(by.css("button.ng-binding.ng-scope")).click();
-  });
+		var img1 = element(by.xpath("//inb-body/ui-view/inb-stars[1]/img[5]")).click();
+		expect(img1.getAttribute('src')).toContain('yellow');
+		var img2 = element(by.xpath("//inb-body/ui-view/inb-stars[2]/img[4]")).click();
+		expect(element(by.xpath("//inb-body/ui-view/inb-stars[2]/img[5]")).getAttribute('src')).toContain('white');
+		var img3 = element(by.xpath("//inb-body/ui-view/inb-stars[4]/img[5]")).click();
+		expect(img3.getAttribute('src')).toContain('yellow');
+		var img4 = element(by.xpath("//inb-body/ui-view/inb-stars[1]/img[5]")).click();
+		expect(img4.getAttribute('src')).toContain('yellow');
+		var img5 = element(by.xpath("//inb-body/ui-view/inb-stars[5]/img[5]")).click();
+		expect(img5.getAttribute('src')).toContain('yellow');
+
+
+	});
+
+	it('test 2', function() {
+		element(by.linkText("Produits")).click();
+		expect(browser.getCurrentUrl())
+			.toBe('http://localhost:8000/app/product');
+
+
+	});
+
+	it('test 3', function() {
+		element(by.linkText("Produits")).click();
+		element(by.css("button.ng-binding.ng-scope")).click();
+
+	});
+
 });

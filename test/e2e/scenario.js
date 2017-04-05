43eda1f95b3c4b12fbf26e1f5b42b4b1156d624c
llesecq <llesecq@inbox.fr>
2017-04-05 16:10:03 +0200

    add: ajout test protractor

--- test/e2e/scenario.js
+++ test/e2e/scenario.js
@@ -3,12 +3,10 @@ describe('Selenium Test Case', function() {
 		browser.get("http://localhost:8000/app/");
 	});
 	it('should execute test case without errors', function() {
-		var text, value, bool, source, url, title;
-		var TestVars = {};
 
 		var img1 = element(by.xpath("//inb-body/ui-view/inb-stars[1]/img[5]")).click();
 		expect(img1.getAttribute('src')).toContain('yellow');
-		var img2 = element(by.xpath("//inb-body/ui-view/inb-stars[2]/img[4]")).click();
+		element(by.xpath("//inb-body/ui-view/inb-stars[2]/img[4]")).click();
 		expect(element(by.xpath("//inb-body/ui-view/inb-stars[2]/img[5]")).getAttribute('src')).toContain('white');
 		var img3 = element(by.xpath("//inb-body/ui-view/inb-stars[4]/img[5]")).click();
 		expect(img3.getAttribute('src')).toContain('yellow');
@@ -31,7 +29,8 @@ describe('Selenium Test Case', function() {
 	it('test 3', function() {
 		element(by.linkText("Produits")).click();
 		element(by.css("button.ng-binding.ng-scope")).click();
-
+		var div = element(by.xpath("//inb-body/ui-view/inb-log/div[7]"));
+		expect(div.getText()).toContain('finished');
 	});
 
 });

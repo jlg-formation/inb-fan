8de74b293a614970ffaf9d2a6cb1688a4a6fd0df
llesecq <llesecq@inbox.fr>
2017-04-05 15:34:23 +0200

    add: protractor

---
+++ test/e2e/scenario.js
@@ -0,0 +1,14 @@
+describe('Selenium Test Case', function() {
+  it('should execute test case without errors', function() {
+    var text, value, bool, source, url, title;
+    var TestVars = {};
+    browser.get("http://localhost:8000/app/");
+    element(by.xpath("//inb-body/ui-view/inb-stars[1]/img[5]")).click();
+    element(by.xpath("//inb-body/ui-view/inb-stars[2]/img[5]")).click();
+    element(by.xpath("//inb-body/ui-view/inb-stars[4]/img[5]")).click();
+    element(by.xpath("//inb-body/ui-view/inb-stars[1]/img[5]")).click();
+    element(by.xpath("//inb-body/ui-view/inb-stars[5]/img[5]")).click();
+    element(by.linkText("Produits")).click();
+    element(by.css("button.ng-binding.ng-scope")).click();
+  });
+});
